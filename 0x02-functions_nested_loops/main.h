b03224fdb103cb5780d98b860073b20b01d40e26
Eze Ernest Chibueze <ezechibuezeernest@gmail.com>
2022-04-12 14:16:44 -0700

    task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H_INCLUDED
-#define MAIN_H_INCLUDED
+#ifndef MAIN_H_
+#define MAIN_H_
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -11,9 +11,8 @@ int _abs(int n);
 int print_last_digit(int n);
 void jack_bauer(void);
 void times_table(void);
-int add(int a, int b);
+int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-
-#endif
+#endif /* _MAIN_H_ */
 ~
