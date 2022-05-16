55fb18fc0e077d552fbbf54d31c83e25ae98f57b
Eze Ernest Chibueze <ezechibuezeernest@gmail.com>
2022-05-16 16:40:34 -0700

    longest day in alx

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,19 +8,20 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list ap;
-	int i;
+	va_list valist;
+	unsigned int i;
 	int sum = 0;
 
 	if (n == 0)
 		return (0);
 
-	va_start(ap, n);
+	va_start(valist, n);
 
 	for (i = 0; i < n; i++)
-		sum += va_arg(ap, int);
+		sum += va_arg(valist, int);
 
-	va_end(ap);
+	va_end(valist);
 
 	return (sum);
 }
+
